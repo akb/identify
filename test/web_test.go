c2f5488dc44a3904c034b4a719a9eb8083f00e38
Alexei Broner <alexei.broner@gmail.com>
2020-09-08 22:16:37 -0500

    Add token form to web test

--- test/web_test.go
+++ test/web_test.go
@@ -3,19 +3,65 @@ package test
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"strings"
 	"testing"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/google/uuid"
 
 	"github.com/akb/identify/cmd/config"
 )
 
 var client *http.Client
+
+const passphrase = "this-will-do-for-now"
+
+func fetch(location string) (*goquery.Document, error) {
+	response, err := client.Get(location)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("expected 200 status code, received %d", response.StatusCode)
+	}
+
+	return goquery.NewDocumentFromReader(response.Body)
+}
+
+func submit(location string, values url.Values) (*goquery.Document, error) {
+	request, err := http.NewRequest(
+		http.MethodPost, location,
+		strings.NewReader(values.Encode()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		msg := fmt.Sprintf("expected 200 status code, received %d\n", response.StatusCode)
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("%s%s", msg, body)
+	}
+
+	return goquery.NewDocumentFromReader(response.Body)
+}
 
 func init() {
 	rootCAs, _ := x509.SystemCertPool()
@@ -25,7 +71,7 @@ func init() {
 
 	certpath, err := config.GetCertificatePath()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	certs, err := ioutil.ReadFile(certpath)
@@ -34,10 +80,16 @@ func init() {
 	}
 
 	if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
-		log.Fatal("no certs appended, using system certs only")
+		log.Fatal("failed to append certificate to root CAs")
+	}
+
+	jar, err := cookiejar.New(&cookiejar.Options{})
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	client = &http.Client{
+		Jar: jar,
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{RootCAs: rootCAs},
 		},
@@ -46,19 +98,10 @@ func init() {
 
 func TestCreateIdentity(t *testing.T) {
 	var csrfToken string
-	var cookies []*http.Cookie
-	{
-		response, err := client.Get("https://localhost:8443/new")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer response.Body.Close()
-		if response.StatusCode != 200 {
-			t.Fatalf("expected 200 status code, received %d", response.StatusCode)
-		}
-		cookies = response.Cookies()
 
-		document, err := goquery.NewDocumentFromReader(response.Body)
+	// get and test new-identity form
+	{
+		document, err := fetch("https://localhost:8443/new")
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -91,45 +134,83 @@ func TestCreateIdentity(t *testing.T) {
 		}
 	}
 
+	// submit new-identity form and test
+	var id string
 	{
-		form := url.Values{
+		document, err := submit("https://localhost:8443/new", url.Values{
 			"csrf_token": []string{csrfToken},
-			"passphrase": []string{"this-will-do-for-now"},
-		}.Encode()
-
-		location := "https://localhost:8443/new"
-		request, err := http.NewRequest(http.MethodPost, location, strings.NewReader(form))
+			"passphrase": []string{passphrase},
+		})
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-		for _, c := range cookies {
-			request.AddCookie(c)
-		}
-
-		response, err := client.Do(request)
-		defer response.Body.Close()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if response.StatusCode != 200 {
-			t.Errorf("expected 200 status code, received %d\n", response.StatusCode)
-			body, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-			t.Fatal(string(body))
-		}
-
-		document, err := goquery.NewDocumentFromReader(response.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if document.Find("[data-testid=id]").Length() == 0 {
+		idElement := document.Find("[data-testid=id]")
+		if idElement.Length() == 0 {
 			t.Fatal("expected POST /new to respond with id of new identity")
 		}
+		id = idElement.First().Text()
+		_, err = uuid.Parse(id)
+		if err != nil {
+			t.Errorf("Failed to parse UUID from: %s\n", id)
+			t.Fatal(err)
+		}
+	}
+
+	// get new-token form and test
+	{
+		document, err := fetch("https://localhost:8443/")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if document.Find("[name=id]").Length() != 1 {
+			t.Fatal("expected new-token form to contain a field named 'id'")
+		}
+
+		if document.Find("[name=passphrase]").Length() != 1 {
+			t.Fatal("expected new-token form to contain a field named 'passphrase'")
+		}
+
+		var exists bool
+		csrfToken, exists = document.Find("[name=csrf_token]").Attr("value")
+		if !exists {
+			t.Fatal("could not find csrf token in new-token form")
+		}
+
+		formElement := document.Find("form")
+		if formElement.Length() != 1 {
+			t.Fatal("expected GET / to respond with a form")
+		}
+		action, _ := formElement.Attr("action")
+		method, _ := formElement.Attr("method")
+		if method != "POST" {
+			t.Fatal("expected GET / to respond with a form with a method of POST")
+		}
+		if action != "/" {
+			t.Fatal("expected GET / to respond with a form with an action of /")
+		}
+
+		if document.Find("[type=submit]").Length() < 1 {
+			t.Fatal("expected GET / to respond with a form includes a submit button")
+		}
+	}
+
+	// submit new-token form
+	{
+		document, err := submit("https://localhost:8443/", url.Values{
+			"csrf_token": []string{csrfToken},
+			"id":         []string{id},
+			"passphrase": []string{passphrase},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		tokenElement := document.Find("[data-testid=token]")
+		if tokenElement.Length() == 0 {
+			t.Fatal("expected POST / to respond with a token")
+		}
+		//token = tokenElement.First().Text()
 	}
 }
